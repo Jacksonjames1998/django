ca65b2d8d3533d94c3d7c726795a48675588812a
Jackson James <jacksonkalluvila@gmail.com>
2021-04-03 14:22:59 +0530

    django template language example

--- shop/products/views.py
+++ shop/products/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>welcome to django...!!!</h1>" )
+    return render(request,'home.html',{"email":"jac@123","name":"jackson"})
 
 def about(request):
     return HttpResponse("<h1>About page</h1>")
 
 def contact(request):
     return HttpResponse("<h1>contact page</h2>")
+
+def product(request):
+    return HttpResponse("<h1>list of products..</h2>")
