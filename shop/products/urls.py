cdf99cdc4eba5ad14021f1587b185b7961fc53ea
Jackson James <jacksonkalluvila@gmail.com>
2021-04-03 15:14:29 +0530

    adding two numbers

--- shop/products/urls.py
+++ shop/products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('about',views.about,name ='about'),
     path('contact',views.contact,name="contact"),
-    path('product',views.product,name="product")
+    path('product',views.product,name="product"),
+    path('add',views.add, name = "add")
 ]
